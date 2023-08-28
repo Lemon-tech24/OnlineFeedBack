008a6c7884ae267dc6053f35984758fa9f433e12
Lemon-tech24 <alisangcocarl@gmail.com>
2023-08-28 18:51:56 +0800

    initial

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
+require('dotenv').config()
+
+
+
+app.use(cors())
+app.use(express.json());
+
+
+//connection to db
+//mongoose.connect(db_url)
 
 app.get('/', (req, res)=>{
     res.send('Working in Port 3000')
 })
+
+
 
 
 app.listen('3000', ()=>{
